d3a497ccf63669baf6f511b5851fcba89de306df
Suraj Kumar <suraj8968625954@gmail.com>
2026-02-15 00:20:23 +0530

    fix port

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import path from "path";
 
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
@@ -21,9 +21,12 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+    origin: process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+    credentials: true,
+})
+
 )
 
 app.use("/api/auth", authRoutes)
